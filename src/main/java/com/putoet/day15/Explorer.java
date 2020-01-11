d979af2cabf5efe64d23bce804bba644947389ba
er21jq <rene.van.bpi.putten@ing.com>
2020-01-11 09:01:06 +0100

    Implemented initial Explorer

---
+++ src/main/java/com/putoet/day15/Explorer.java
@@ -0,0 +1,52 @@
+package com.putoet.day15;
+
+import com.putoet.day9.IOutputDevice;
+
+public class Explorer {
+    public static final int BLOCKED = 0;
+    public static final int MOVED = 1;
+    public static final int HIT_AIR = 2;
+
+    private final Navigator navigator;
+    private final IOutputDevice outputDevice;
+    private final IExtendableSurface surface;
+
+    public Explorer(IExtendableSurface surface, Navigator navigator, IOutputDevice outputDevice) {
+        this.surface = surface;
+        this.navigator = navigator;
+        this.outputDevice = outputDevice;
+    }
+
+    public void explore() {
+        navigator.north();
+        checkStatus();
+    }
+
+    private void checkStatus() {
+        switch (outputDevice.get().intValue()) {
+            case MOVED:
+                if (navigator.currentTile().type() == Tile.Type.UNKNOWN) {
+                    navigator.currentTile().discovered(Tile.Type.OPEN);
+                    surface.paint(navigator.currentPoint(), navigator.currentTile());
+                }
+                break;
+
+            case HIT_AIR:
+                if (navigator.currentTile().type() == Tile.Type.UNKNOWN) {
+                    navigator.currentTile().discovered(Tile.Type.AIR);
+                    surface.paint(navigator.currentPoint(), navigator.currentTile());
+                }
+                break;
+
+            case BLOCKED:
+            default:
+                if (navigator.currentTile().type() == Tile.Type.UNKNOWN) {
+                    navigator.currentTile().discovered(Tile.Type.WALL);
+                    surface.paint(navigator.currentPoint(), navigator.currentTile());
+                }
+                navigator.back();
+        }
+    }
+}
+
+
---
+++ src/test/java/com/putoet/day15/TestExplorer.java
@@ -0,0 +1,27 @@
+package com.putoet.day15;
+
+import com.putoet.day9.IInputDevice;
+import com.putoet.day9.IOutputDevice;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+public class TestExplorer {
+
+    @Test
+    public void testExplore() {
+        final IExtendableSurface surface = new OriginExtendableSurface();
+        final IOutputDevice outputDevice = mock(IOutputDevice.class);
+        final IInputDevice inputDevice = mock(IInputDevice.class);
+        final Navigator navigator = new Navigator(inputDevice, new ArrayList<Long>());
+        final Explorer explorer = new Explorer(surface, navigator, outputDevice);
+
+        when(outputDevice.get()).thenReturn((long) Explorer.BLOCKED);
+        explorer.explore();
+        assertEquals(Point.ORIGIN, navigator.currentPoint());
+        assertEquals(Tile.Type.WALL, navigator.currentTile().north().type());
+    }
+}
