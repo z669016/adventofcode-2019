93aa8369f230a0c49953510770e4e74fb3a025d4
Rene van Putten <z669016@gmail.com>
2023-09-01 12:53:31 +0300

    Added timing, introduced var, and reduced scope where possible, minor code enhancements

--- src/main/java/com/putoet/day5/Day5.java
+++ src/main/java/com/putoet/day5/Day5.java
@@ -2,26 +2,23 @@ package com.putoet.day5;
 
 import com.putoet.intcode.*;
 import com.putoet.resources.CSV;
+import com.putoet.utils.Timer;
 
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Queue;
-import java.util.concurrent.BlockingDeque;
-import java.util.concurrent.LinkedBlockingDeque;
 
 public class Day5 {
     public static void main(String[] args) {
-        final List<Long> intCode = CSV.list("/day5.txt", Long::parseLong).get(0);
+        final var intCode = CSV.list("/day5.txt", Long::parseLong).get(0);
 
-        part1(intCode);
-        part2(intCode);
+        Timer.run(() -> part1(intCode));
+        Timer.run(() -> part2(intCode));
     }
 
     private static void part1(List<Long> intCode) {
-        final Memory memory = new FixedMemory(intCode);
-        final IntCodeInputOutputDevice input = new IntCodeInputOutputDevice();
-        final IntCodeInputOutputDevice output = new IntCodeInputOutputDevice();
-        final IntCodeDevice device = IntCodeComputer.builder().memory(memory).input(input).output(output).build();
+        final var memory = new FixedMemory(intCode);
+        final var input = new IntCodeInputOutputDevice();
+        final var output = new IntCodeInputOutputDevice();
+        final var device = IntCodeComputer.builder().memory(memory).input(input).output(output).build();
 
         input.offer(1L);
         device.run();
@@ -30,10 +27,10 @@ public class Day5 {
     }
 
     private static void part2(List<Long> intCode) {
-        final Memory memory = new FixedMemory(intCode);
-        final IntCodeInputOutputDevice input = new IntCodeInputOutputDevice();
-        final IntCodeInputOutputDevice output = new IntCodeInputOutputDevice();
-        final IntCodeDevice device = IntCodeComputer.builder().memory(memory).input(input).output(output).build();
+        final var memory = new FixedMemory(intCode);
+        final var input = new IntCodeInputOutputDevice();
+        final var output = new IntCodeInputOutputDevice();
+        final var device = IntCodeComputer.builder().memory(memory).input(input).output(output).build();
 
         input.offer(5L);
         device.run();
